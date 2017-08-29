d59ff50af03d686cd42ce4b41f8291dff81fcdd8
Wilson Alves <wirsus@icloud.com>
2017-08-29 10:15:01 -0300

    Adicionar arquivos de projeto.

---
+++ CabineBarbearia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CabineBarbearia.MvcApplication" Language="C#" %>
---
+++ CabineBarbearia/Models/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CabineBarbearia.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [Display(Name = "Código Cliente")]
+        public int id_cliente { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Informe o nome do cliente!")]
+        [StringLength(100, ErrorMessage = "O nome deve conter no máximo {1} caracteres!"), MinLength(5, ErrorMessage = "O nome deve conter no mínimo {1} caracteres!")]
+        public String nm_cliente { get; set; }
+
+        [Display(Name = "Apelido")]
+        [StringLength(100, ErrorMessage = "O apelido deve conter no máximo {1} caracteres!"), MinLength(2, ErrorMessage ="O apelido deve conter no mínimo {1} caracteres!")]
+        public String nm_apelido { get; set; }
+
+        [Display(Name = "Celular")]
+        public String nr_celular { get; set; }
+
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "Informe um e-mail válido!")]
+        public String dc_email { get; set; }
+
+        [Display(Name = "Data de Cadastro")]
+        [DisplayFormat(DataFormatString = ("{0:d}"), ApplyFormatInEditMode = true)]
+        public DateTime dt_cadastro { get; set; }
+
+        [Display(Name = "Status do Cliente")]
+        public bool id_status { get; set; }
+
+    }
+}
